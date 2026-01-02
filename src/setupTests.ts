c1107f629e0b57fbdc660c1d199c19205583004c
nicola-palo <nicolapiero.palo@gmail.com>
2026-01-02 16:55:14 +0100

    changed the title, the favicon and increased the transparency of the navbar

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,17 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-
-Object.defineProperty(window, 'matchMedia', {
-	writable: true,
-	value: (query: string) => ({
-		matches: false,
-		media: query,
-		onchange: null,
-		addListener: () => {},
-		removeListener: () => {},
-		addEventListener: () => {},
-		removeEventListener: () => {},
-		dispatchEvent: () => false,
-	}),
-});
